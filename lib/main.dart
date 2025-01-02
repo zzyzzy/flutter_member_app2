ca90b77888b3613866fe059dd1bad8f0ca8d62f0
zzyzzy <zzyzigy@gmail.com>
2025-01-02 12:10:42 +0900

    sqflite_common_ffi_web 설정 insertMember 기능 구현

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:sqflite_common_ffi_web/sqflite_ffi_web.dart';
+
+import 'helper/database_helper.dart';
+import 'model/member.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Window, Linux 앱 실행시
+  // databaseFactory = databaseFactoryFfi;
+  // sqfliteFfiInit();
+
+  // web 앱 실행시
+  // dart run sqflite_common_ffi_web:setup  (터미널)
+  databaseFactory = databaseFactoryFfiWeb;
+
   runApp(const MyApp());
 }
 
@@ -32,8 +47,16 @@ class MainActivity extends StatefulWidget {
 
 class _MainActivityState extends State<MainActivity> {
 
+  // 변수 정의
+  final Database_Helper _dbHelper = Database_Helper.instance;
+  final TextEditingController _useridController = TextEditingController();
+  final TextEditingController _passwdController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _nameController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
@@ -46,10 +69,10 @@ class _MainActivityState extends State<MainActivity> {
               SizedBox(height: 150,),
               Text('회원가입 앱', style: TextStyle(fontSize: 34), textAlign: TextAlign.center,),
               SizedBox(height: 150,),
-              TextField(decoration: const InputDecoration(hintText: '아이디 입력'),),
-              TextField(decoration: const InputDecoration(hintText: '비밀번호 입력'),),
-              TextField(decoration: const InputDecoration(hintText: '이메일 입력'),),
-              TextField(decoration: const InputDecoration(hintText: '이름 입력'),),
+              TextField(controller: _useridController, decoration: const InputDecoration(hintText: '아이디 입력'),),
+              TextField(controller: _passwdController, decoration: const InputDecoration(hintText: '비밀번호 입력'),),
+              TextField(controller: _emailController, decoration: const InputDecoration(hintText: '이메일 입력'),),
+              TextField(controller: _nameController, decoration: const InputDecoration(hintText: '이름 입력'),),
               SizedBox(height: 16,),
               ElevatedButton(onPressed: _registerUser, child: Container(
                 width: double.infinity, child: Text('회원 가입', textAlign: TextAlign.center,),)),
@@ -65,10 +88,37 @@ class _MainActivityState extends State<MainActivity> {
 
   // 회원가입 처리
   Future<void> _registerUser() async {
-    // showSnackBar : 간단한 메세지 출력
-    ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(content: Text('회원가입 기능 구현중...'))
+    // 입력한 회원 정보 가져오기
+    String userid = _useridController.text.trim();
+    String passwd = _passwdController.text.trim();
+    String email = _emailController.text.trim();
+    String name = _nameController.text.trim();
+
+    // 입력 내용 확인
+    if (userid.isEmpty || passwd.isEmpty || email.isEmpty || name.isEmpty) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('모든 필드를 입력하세요!!'))
+      );
+    }
+
+    // 저장할 회원 객체 생성
+    final member = Member(
+      userid: userid, passwd: passwd, email: email, name: name
     );
+
+    // 회원 정보 저장 처리
+    final result = await _dbHelper.insertMember(member);
+    if (result > 0) {
+      ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('회원가입이 완료되었습니다!!'))
+      );
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('회원가입 실패!!'))
+      );
+    }
+
+
   }
 
   // 회원조회 처리
