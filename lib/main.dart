e9ed146742f5ad28306f3e563a33757156afb3bd
zzyzzy <zzyzigy@gmail.com>
2025-01-02 10:35:23 +0900

    회원가입/조회 앱 UI 구현

--- lib/main.dart
+++ lib/main.dart
@@ -39,14 +39,43 @@ class _MainActivityState extends State<MainActivity> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text('Hello, World!!', style: Theme.of(context).textTheme.headlineLarge),
-          ],
+      body: Padding(padding: const EdgeInsets.all(16),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(height: 150,),
+              Text('회원가입 앱', style: TextStyle(fontSize: 34), textAlign: TextAlign.center,),
+              SizedBox(height: 150,),
+              TextField(decoration: const InputDecoration(hintText: '아이디 입력'),),
+              TextField(decoration: const InputDecoration(hintText: '비밀번호 입력'),),
+              TextField(decoration: const InputDecoration(hintText: '이메일 입력'),),
+              TextField(decoration: const InputDecoration(hintText: '이름 입력'),),
+              SizedBox(height: 16,),
+              ElevatedButton(onPressed: _registerUser, child: Container(
+                width: double.infinity, child: Text('회원 가입', textAlign: TextAlign.center,),)),
+              SizedBox(height: 16,),
+              ElevatedButton(onPressed: _listUsers, child: Container(
+                width: double.infinity, child: Text('회원 조회', textAlign: TextAlign.center,),)),
+            ],
+          ),
         ),
-      ),
+      )
+    );
+  } // build
+
+  // 회원가입 처리
+  Future<void> _registerUser() async {
+    // showSnackBar : 간단한 메세지 출력
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text('회원가입 기능 구현중...'))
     );
   }
-}
+
+  // 회원조회 처리
+  Future<void> _listUsers() async {
+    ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('회원조회 기능 구현중...'))
+    );
+  }
+
+} // _MainActivityState
